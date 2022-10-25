8e9b95aa79317327e435d0a7f7d8e94f1525cc24
MicheleUIT <mgu111@uit.no>
2022-10-25 17:09:52 +0200

    update

--- code/bayesian/models.py
+++ code/bayesian/models.py
@@ -4,9 +4,9 @@ import pyro
 import pyro.distributions as dist
 
 from pyro.nn import PyroModule, PyroSample
-from pyro.infer.autoguide import AutoMultivariateNormal, init_to_mean
-from pyro.optim import Adam
-from pyro.infer import SVI, Trace_ELBO, Predictive, MCMC, NUTS, HMC
+# from pyro.infer.autoguide import AutoMultivariateNormal, init_to_mean
+# from pyro.optim import Adam
+# from pyro.infer import SVI, Trace_ELBO, Predictive, MCMC, NUTS, HMC
 
 
 class BayesianModel(PyroModule):
@@ -15,24 +15,44 @@ class BayesianModel(PyroModule):
 
         self.device = device
         self.config = config
-        
-        pyro.nn.module.to_pyro_module_(torch_model)
         self.model = torch_model
+
+        self.prior_dist = []
+
+        for prior in self.config.prior:
+            if prior["name"] == "Gauss":
+                self.prior_dist.append(dist.Normal(torch.tensor(prior["params"][0], device=self.device),
+                                            torch.tensor(prior["params"][1], device=self.device)))
+            elif prior["name"] == "Uniform":
+                self.prior_dist.append(dist.Uniform(prior["params"][0], prior["params"][1]))
+            else:
+                raise ValueError("Undefined prior distribution.")
         
+        self.torch2pyro()
+
+
+    def torch2pyro(self):
+        pyro.nn.module.to_pyro_module_(self.model)
+
         for m in self.model.modules():
             for name, value in list(m.named_parameters(recurse=False)):
-                setattr(m, name, PyroSample(dist.Normal(torch.tensor(0., device=self.device), torch.tensor(1., device=self.device))
-                                                                .expand(value.shape).to_event(value.dim())))
+                setattr(m, name, PyroSample(self.prior_dist[0].expand(value.shape).to_event(value.dim())))
+
 
     def forward(self, x, y=None):
-        sigma = pyro.sample("sigma", dist.Uniform(0., 10.)).to(self.device)
         mean = self.model(x).squeeze(-1)
+
+        sigma = pyro.sample("sigma", self.prior_dist[1]).to(self.device)
+
         with pyro.plate("data", x.shape[0], device=self.device):
+            # should I change also the likelihood?
             obs = pyro.sample("obs", dist.Normal(mean, sigma), obs=y)
         return mean
 
+
     def render_model(self, model_args):
-        pyro.render_model(self, model_args, render_distributions=True)
+        return pyro.render_model(self, model_args, render_distributions=True)
     
 
 
+# when defining the guide, put constraints on variances
---
+++ code/main.py
@@ -0,0 +1,42 @@
+import torch
+import wandb
+import os
+
+from bayesian.models import BayesianModel
+
+config = {
+            "prior": # ordered list of priors
+                [{
+                    "name" : "Gauss",
+                    "params" : [0., 1.]
+                },
+                {
+                    "name" : "Uniform",
+                    "params" : [0., 10.]
+                }],
+            }
+
+os.environ["WANDB_MODE"]="offline"
+wandb.init(project="bayes_rc", config=config)
+config = wandb.config
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+
+def main():
+
+    n_dims = 20
+    in_features = n_dims
+    out_features = 1
+    torch_model = torch.nn.Sequential(
+                    torch.nn.Linear(in_features, 10),
+                    torch.nn.Tanh(),
+                    torch.nn.Linear(10, out_features)
+                ).to(device)
+
+    model = BayesianModel(torch_model, config, device)
+    model.render_model(model_args=(torch.rand(1,20), torch.rand(1,1))) # how to print it?
+
+
+if __name__ == "__main__":
+    main()
