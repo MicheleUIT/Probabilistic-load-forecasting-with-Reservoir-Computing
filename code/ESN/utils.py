b936ae43efdadc4dd0a4c7e2dc95111fafe8b0fe
MicheleUIT <mgu111@uit.no>
2022-11-03 17:17:05 +0100

    + esn utils

---
+++ code/ESN/utils.py
@@ -0,0 +1,23 @@
+import json
+import esnet
+
+from dataset.data_loaders import load_dataset, generate_datasets
+
+
+def run_esn(dataset):
+    X, Y = load_dataset(dataset)
+
+    # Set ESN hyperparams
+    config = json.load(open('ESN/configs/ESN_hyperparams.json', 'r'))
+
+    Xtr, Ytr, Xval, Yval, Xte, Yte = generate_datasets(X, Y, test_percent = 0.15, val_percent = 0.15)
+    print("Tr: {:d}, Val: {:d}, Te: {:d}".format(Xtr.shape[0], Xval.shape[0], Xte.shape[0]))
+
+    # Train and compute predictions
+    Yte_pred, _, _, train_embedding, _, val_embedding, _, test_embedding = esnet.run_from_config_return_states(Xtr, Ytr, 
+                                                                                                                Xte, Yte, 
+                                                                                                                config, 
+                                                                                                                validation=True,
+                                                                                                                Xval=Xval,
+                                                                                                                Yval=Yval)
+    return Ytr, train_embedding, val_embedding, test_embedding
