adca64b7e1812a0d2225662163a23068c8bf85e6
MicheleUIT <mgu111@uit.no>
2023-01-26 11:36:57 +0100

    + spain dataset

--- code/dataset/data_loaders.py
+++ code/dataset/data_loaders.py
@@ -7,12 +7,14 @@ from sklearn.preprocessing import StandardScaler
 def load_dataset(name):
     if name == "acea":
         return load_acea()
+    elif name == "spain":
+        return load_spain()
     else:
         raise ValueError(f"{name} dataset not defined.")
 
 
 def load_acea():
-    forecast_horizon = 24 
+    forecast_horizon = 24 # 1 day
 
     mat = loadmat('dataset/TS_Acea.mat')  # load mat-file
     ACEA_data = mat['X'] # original resolution (1 = 10 mins)
@@ -21,6 +23,24 @@ def load_acea():
 
     X = ACEA_data[:-forecast_horizon]
     Y = ACEA_data[forecast_horizon:]
+
+    return X, Y
+
+
+def load_spain():
+    """
+    Spanish energy market daily data.
+    Source: https://www.kaggle.com/code/manualrg/daily-electricity-demand-forecast-machine-learning/data
+    """
+    forecast_horizon = 7 # 1 week
+
+    spain_power_data = np.genfromtxt('dataset/spain_energy_market.csv', delimiter=',', dtype=None, encoding=None)
+    data = spain_power_data[...,5] # select column with values
+    data = data[spain_power_data[...,2] == 'Demanda real'] # select energy demand values
+    data = data.astype(float) # convert into floats
+
+    X = data[:-forecast_horizon, np.newaxis]
+    Y = data[forecast_horizon:, np.newaxis]
 
     return X, Y
 
