2f354ff7450b69fecdb88047249263d0098bd1bf
MicheleUIT <mgu111@uit.no>
2023-06-01 16:44:56 +0200

    fix plot

--- inference/frequentist/utils.py
+++ inference/frequentist/utils.py
@@ -17,14 +17,14 @@ def compute_coverage_len(y_test, y_lower, y_upper):
     return coverage, avg_length
 
 
-def plot_forecast(predictive, Y):
+def plot_forecast(predictive, Y, diffXte, diffYte, length=200):
     mean_index = int(predictive.shape[1]/2)
     q_low, mean, q_hi = predictive[:,2].cpu().numpy(), predictive[:,mean_index].cpu().numpy(), predictive[:,-2].cpu().numpy()
 
     fig = plt.figure(figsize=(15,5))
-    plt.plot(Y.cpu()[:200], label='true value', color='k')
-    plt.fill_between(np.arange(mean.shape[0])[:200], q_low[:200], q_hi[:200], alpha=0.3, label='0.95 PI')
-    plt.plot(mean[:200], label='prediction')
+    plt.plot((Y.cpu()+diffYte)[:length], label='true value', color='k')
+    plt.fill_between(np.arange(mean.shape[0])[:length], (q_low+diffYte)[:length], (q_hi+diffYte)[:length], alpha=0.3, label='0.95 PI')
+    plt.plot((mean+diffYte)[:length], label='prediction')
     plt.legend(loc='best', fontsize=10)
     plt.grid()
 
